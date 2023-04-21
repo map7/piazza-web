f81f8544f9f3f193d978572bed4f0c29abf721a0
Michael Pope <michael@dtcorp.com.au>
2023-04-21 15:00:14 +1000

    Add users & feeds controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'feed/show'
+  get 'users/new'
+  get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

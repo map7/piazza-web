7dc3ee6e9d60352ea6bd3a55a31d8935198cc14c
Michael Pope <michael@dtcorp.com.au>
2023-04-26 16:23:25 +1000

    Add login route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get "sign_up", to: 'users#new'
   post "sign_up", to: 'users#create'
+
+  get "login", to: "sessions#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

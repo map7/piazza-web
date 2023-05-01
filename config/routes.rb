82e8e92db84cda948a8bf3b5e1fddac4875d7849
Michael Pope <michael@dtcorp.com.au>
2023-05-01 16:10:35 +1000

    Add login post route & method

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post "sign_up", to: 'users#create'
 
   get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

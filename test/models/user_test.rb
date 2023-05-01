03271d6cfd8d815d1a98baff7fbabb1ac7149aac
Michael Pope <michael@dtcorp.com.au>
2023-05-01 16:15:53 +1000

    Test we create an app session when correct details are entered

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,4 +62,32 @@ class UserTest < ActiveSupport::TestCase
 
   end
 
+  test "can create a session with email and correct password" do
+    @app_session = User.create_app_session(
+      email: "jerry@example.com",
+      password: "password"
+    )
+
+    assert_not_nil @app_session
+    assert_not_nil @app_session.token
+  end
+
+  test "cannot create a session with email and incorrect password" do
+    @app_session = User.create_app_session(
+      email: "jerry@example.com",
+      password: "WRONG"
+    )
+
+    assert_nil @app_session
+  end
+
+  test "creating a session with non existent email returns nil" do
+    @app_session = User.create_app_session(
+      email: "whoami@example.com",
+      password: "WRONG"
+    )
+
+    assert_nil @app_session
+  end
+
 end

d9b679c0e6eee43e08b512ebc854d1ec70c425a8
Michael Pope <michael@dtcorp.com.au>
2023-05-12 14:58:32 +1000

    User-REFACTOR-Move authentication logic to a concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Authentication
+
   has_many :memberships, dependent: :destroy
   has_many :organisations, :through => :memberships
 
@@ -8,25 +10,6 @@ class User < ApplicationRecord
     uniqueness: {case_sensitive: false}
 
   before_validation :strip_extraneous_spaces
-
-  has_secure_password
-  validates :password,
-    presence: true,
-    length: {minimum: 8}
-
-  has_many :app_sessions
-
-  def self.create_app_session(email:, password:)
-    return nil unless user = User.find_by(email: email.downcase)
-
-    user.app_sessions.create if user.authenticate(password)
-  end
-
-  def authenticate_app_session(app_session_id, token)
-    app_sessions.find(app_session_id).authenticate_token(token)
-  rescue ActiveRecord::RecordNotFound
-    nil
-  end
 
   private
 

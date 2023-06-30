898238de39150ef8119dba1addbd938fff8e39bb
Michael Pope <michael@dtcorp.com.au>
2023-06-30 15:04:19 +1000

    Stimulus-Create navbar controller
    Remember to start with 'bin/dev'

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  connect() {
+
+  static targets = ["burger", "menu"]
+ 
+  toggle() {
+    this.burgerTarget.classList.toggle("is-active")
+    this.menuTarget.classList.toggle("is-active")
   }
 }

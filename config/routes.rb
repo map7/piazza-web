3ce285e5689bada0049e6e607d5ff7311cd827f3
Michael Pope <michael@dtcorp.com.au>
2023-07-07 16:02:42 +1000

    Logout-FEAT-Create logout code

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
-  delete "logout", to: "session#destroy"
+  delete "logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

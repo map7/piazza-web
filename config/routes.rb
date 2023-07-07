b3106b368dabbad119b7bd513761f28cf26939da
Michael Pope <michael@dtcorp.com.au>
2023-07-07 15:55:38 +1000

    Logout-TEST-Write logout test and add route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
+  delete "logout", to: "session#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
